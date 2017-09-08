bf7da1d97f93f4c980c3616946441fc79c8b419a
tnfaid <nurfaidahtitik@gmail.com>
2017-09-08 14:33:27 +0700

    akses dan ubah value pada array

--- js/array-constructor.js
+++ js/array-constructor.js
@@ -2,3 +2,15 @@ var colors = new Array('w', 'r', 'g');
 
 var elColor = document.getElementById('colors');
 elColor.innerHTML = colors.item(0);
+
+// create array
+var colors = ['white', 'black', 'custom'];
+
+// update the third item in the array
+colors[2] = 'customJadi';
+
+// get the element with an id of colors
+var el = document.getElementById('colors');
+
+// replace with third item from the array
+el.textContent = colors[2];
