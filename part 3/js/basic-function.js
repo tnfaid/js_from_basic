89371e2bd1fa6df1b721bc84bb7f9fceb077ae51
tnfaid <nurfaidahtitik@gmail.com>
2017-09-11 11:40:29 +0700

    membuat object menggunakan notasi literal

---
+++ part 3/js/basic-function.js
@@ -0,0 +1,7 @@
+var a= "diskon weee";
+
+function updateA(){
+	var elA = document.getElementById('message');
+	elA.textContent = a;
+}
+updateA();
---
+++ part 3/js/function-wrinting.js
@@ -0,0 +1,5 @@
+var area= (function(){
+	var width = 3;
+	var height = 2;
+	return width * height;
+}());
